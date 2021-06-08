5a371ef4f59bc92dafdaf310bd6db4d0b5911324
Moath alluqmani <48844900+moathallqumani@users.noreply.github.com>
2021-06-08 07:15:33 +0300

    Email checking step 1 section 6 lesson 2

---
+++ SDAemailChecking/src/emailChecking.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class emailChecking {
+
+	static Scanner scan = new Scanner(System.in);
+
+
+public static void main(String[] args) {
+		emailCheck();
+	}
+
+static void emailCheck() {
+	
+   String [] arr = {"m@hotmail.com","a@hotmail.com","b@hotmail.com"};
+   for (int i = 0; i < arr.length; i++) {
+	if(arr[i]=="a@hotmail.com") { 
+		System.out.println("Founded! ");
+       break;}
+	
+}
+   
+}
+}
